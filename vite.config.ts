d53a05e130133cd6bce26c0cde9ee7bba3f428f5
chienzxzx33 <chienzxzx33@gmail.com>
2024-02-25 16:49:05 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/fabbi-test',
     build: {
         outDir: 'build', // Specify the output directory
         assetsDir: 'assets', // Specify the directory for assets (like images, fonts, etc.)
